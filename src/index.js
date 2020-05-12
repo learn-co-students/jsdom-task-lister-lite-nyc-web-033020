dc63f8608b59b0df371a7c8adb4e6703e51bb6b2
github username <github email address>
2020-05-12 17:18:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const todoForm = document.getElementById('create-task-form') 
+  const todoInput = document.getElementById('new-task-description')
+  const list = document.querySelector('#tasks')
+  let userInput
+  const dropDown = document.querySelector('#priority')
+  const priority = document.querySelectorAll('option')
+
+  todoForm.addEventListener('submit', function(event){
+    
+    event.preventDefault()
+
+    let taskItem = document.createElement('li')
+
+    if(dropDown.value==='low'){
+      taskItem.style.color = "green"
+    } else if (dropDown.value==='medium'){
+      taskItem.style.color = "yellow"
+    } else if (dropDown.value==='high'){
+      taskItem.style.color = "red"
+    }
+
+    userInput = todoInput.value
+    taskItem.textContent = userInput
+    list.appendChild(taskItem)
+    todoForm.reset()
+    
+    let deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '&times;'
+    taskItem.appendChild(deleteButton)
+
+    deleteButton.className = 'delete'
+    const deleteButtons = document.getElementsByClassName("delete")
+    const deleteArray = Array.from(deleteButtons)
+  
+    deleteArray.forEach(function(button){
+      button.addEventListener('click', function(event){
+        event.target.parentElement.remove()
+      })
+    })    
+  });
 });
