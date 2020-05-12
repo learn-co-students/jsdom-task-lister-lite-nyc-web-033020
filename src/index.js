2028be81602ad6de4222f43fabd6ad35d59ef4f8
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-12 14:09:38 -0700

    basic functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskInput = document.getElementById('new-task-description')
+
+  let inputButton = document.getElementsByTagName('input')[1]
+
+  let formText = ""
+  
+  taskInput.addEventListener('input', function(e) {
+    let inputText = e.target.value
+    formText = inputText
+  });
+ 
+  inputButton.addEventListener('click', function(e){
+    
+    e.preventDefault();
+
+    let li = document.createElement('li')
+    let deleteButton = document.createElement('button')
+    li.innerText = formText;
+    deleteButton.innerText = 'x'
+    
+
+    let tasks = document.getElementById('tasks')
+    li.appendChild(deleteButton)
+    tasks.appendChild(li)
+    
+
+
+    let form = document.getElementById('create-task-form')
+    form.reset()
+
+  });
+
 });
