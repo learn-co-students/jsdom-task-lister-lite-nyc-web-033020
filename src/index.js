c26974c25911b7d163fba2fb4d41da8c17c07246
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-12 16:55:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   submitButton.addEventListener('click', function(e){
     e.preventDefault();
 
+    // get the text box
     let textBox = document.getElementById('new-task-description');
 
     // create an empty li
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // color p's text
     const priority = document.getElementById('priority')
     if (priority.value == "high") p.style.color = "red";
-    else if (priority.value == "medium") p.style.color = "yellow";
+    else if (priority.value == "medium") p.style.color = "orange";
     else p.style.color = "green";
 
     // add delete button to li
@@ -29,8 +30,39 @@ document.addEventListener("DOMContentLoaded", () => {
       event.target.parentElement.remove();
     });
 
-    // attach the li to the unordered list
-    let tasks = document.getElementById('tasks');
-    tasks.appendChild(li);
+    // add text field
+    const textField = document.createElement('input');
+    textField.className = "editb";
+    textField.type = "text";
+    li.appendChild(textField);
+
+    // add edit button
+    const editButton = document.createElement('button');
+    editButton.innerText = "edit";
+    li.appendChild(editButton);
+    editButton.addEventListener('click', function(e) {
+      console.log(e);
+      const newText = document.getElementsByClassName('editb')[0].value;
+      p.innerText = newText;
+    });
+
+    // order tasks
+    // saved into red, orange, green seperate lists
+    if (priority.value == "high") {
+
+      let redTasks = document.getElementById("red-tasks");
+      redTasks.appendChild(li);
+
+    } else if (priority.value == "medium") {
+
+      let orangeTasks = document.getElementById("orange-tasks");
+      orangeTasks.appendChild(li);
+
+    } else if (priority.value == "low") {
+
+      let greenTasks = document.getElementById("green-tasks")
+      greenTasks.appendChild(li);
+
+    }
   });
 });
